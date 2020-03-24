84f3202bca72b1e0c4d9eda2d633e8eb966998bc
dvranga <devangmranganth>
2020-03-24 09:33:15 +0530

    Sorting in Descending order

--- ArthmeticandSorting.sh
+++ ArthmeticandSorting.sh
@@ -25,4 +25,20 @@ values[ ((counter++)) ]=${value[FirstComputation]}
 values[ ((counter++)) ]=${value[SecondComputation]}
 values[ ((counter++)) ]=${value[ThirdComputation]}
 values[ ((counter++)) ]=${value[FourthComputation]}
+echo elements in values
+echo ${values[@]}
+echo sorting the elemets
+temp=0
+for (( i = 0; i < 4 ; i++ ))
+do
+   for (( j = $i; j < 4; j++ ))
+   do
+      if [ ${values[$i]} -lt ${values[$j]} ]; then
+           t=${values[$i]}
+           values[$i]=${values[$j]}
+           values[$j]=$t
+     fi
+   done
+done
+echo "Array in sorted order :"
 echo ${values[@]}
