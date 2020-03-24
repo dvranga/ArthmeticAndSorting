00d7e0053f13625b5587700ea7bc1a2bdcdfa7f2
dvranga <devangmranganth>
2020-03-24 08:07:21 +0530

    Storing results into the Dictionaries

--- ArthmeticandSorting.sh
+++ ArthmeticandSorting.sh
@@ -5,4 +5,16 @@ echo enter the second value
 read b
 echo enter the third value
 read c
-echo $(( $c % $a + $b ))
+echo $(( $a % $(($b + $c)) ))
+echo $(( $a + $(($b * $c)) ))
+echo $(( $a * $(($b + $c)) ))
+echo $(( $c + $(($a / $b)) ))
+declare -A value
+value[FirstComputation]=$(( $a % $(($b + $c)) ))
+value[SecondComputation]=$(( $a + $(($b * $c)) ))
+value[ThirdComputation]=$(( $a * $(($b + $c)) ))
+value[FourthComputation]=$(( $c + $(($a / $b)) ))
+echo "FirstComputation" ${value[FirstComputation]}
+echo "SecondComputation" ${value[SecondComputation]}
+echo "ThirdComputation" ${value[ThirdComputation]}
+echo "FourthComputation" ${value[FourthComputation]}
